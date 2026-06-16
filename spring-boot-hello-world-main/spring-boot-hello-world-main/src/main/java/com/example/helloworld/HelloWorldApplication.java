b54928232f31488b5522ebf7913f2f66076e747c
bhushanbihani <bhushanbihani@gmail.com>
2026-06-16 15:32:20 +0530

    Update HelloWorldApplication.java

--- spring-boot-hello-world-main/spring-boot-hello-world-main/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ spring-boot-hello-world-main/spring-boot-hello-world-main/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
+        String password="Bhushan@123";
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
